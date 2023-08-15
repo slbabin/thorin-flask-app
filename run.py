9bac0171dd39c3ae064a24f54f2e198d3acc7ffd
slbabin <>
2023-08-15 20:20:40 +0000

    Add more pages

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 @app.route("/contact")
 def contact():
