1250b9f627116faf820ebc8aeb471d0a43b885fc
slbabin <>
2023-08-17 11:19:09 +0000

    deplyement to heroku

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
-if os.path.exists("env.py")
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     data = []
@@ -19,15 +21,17 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
     with open("static/data/company.json", "r") as json_data:
         data = json.load(json_data)
-        for obj in data: 
+        for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return  render_template("member.html", member=member)         
+    return render_template("member.html", member=member)
+
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
@@ -36,15 +40,14 @@ def contact():
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
+
 @app.route("/careers")
 def careers():
-    return render_template("careers.html", page_title="Careers")     
+    return render_template("careers.html", page_title="Careers")
 
-if __name__ =="__main__":
-    
-    
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-    
